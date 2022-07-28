f4c4ef3365bb5df2c5e2eb0822df018c3e5ab8fd
Omer_Cebeci <62637754+omr-web@users.noreply.github.com>
2022-07-28 12:01:19 +0300

    Add files via upload

---
+++ If_Statement.c
@@ -0,0 +1,33 @@
+// usage of If control statement
+
+/*
+int main () {
+    int x=10 , y=20;
+
+    if(x || y++)   // || yerine | yaz, ==> y 20 değil bu sefer 21 olacak çünkü bitsel | de kısa devre yok 
+        printf("dogru");
+    
+    else 
+        printf("yanlis");
+    printf("y: %d ",y);
+
+
+}
+*/
+
+
+/*
+int main() {
+	int x = 5;
+
+
+	if (--x < 5) // x-- <5 olsa idi ?
+		printf("%d \n ", x),printf("dogru");
+	else
+		printf("%d \n ", x + 5),printf("yanlis");
+
+
+
+}
+*/
+
---
+++ comma_operator_usage.c
@@ -0,0 +1,40 @@
+
+//Comma oparator using
+
+/*
+int main () {
+    int x=10 , y=30;
+    int temp;
+
+    // normalde
+    
+    //temp=x;
+    //x=y;
+    //y=temp;
+    //printf("x: %d \n ",x);
+    //printf("y: %d \n ",y);
+    
+    // 2. yöntem 
+    temp=x, x=y, y=temp ;
+    printf("x: %d \n ",x);
+    printf("y: %d \n ",y);
+
+
+
+}
+*/
+
+/*
+int main () {
+    int x=10, y=30;
+    int temp ;
+
+    temp=x++ , y=++x;
+
+    printf(" x: %d \n ",x); //12
+    printf(" y: %d \n ",y); // 12
+    printf(" temp: %d \n ",temp); // 10
+}
+
+*/
+
